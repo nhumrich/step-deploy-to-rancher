c052512dd4bd98e5b5425ed305d31340c3a33f1a
Nick H <nick.humrich@canopytax.com>
2015-10-20 14:50:33 -0600

    Make it pretty

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ mkdir "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME"
 function get_env_id { curl -s "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY:$WERCKER_DEPLOY_TO_RANCHER_SECRET_KEY@$WERCKER_DEPLOY_TO_RANCHER_RANCHER_URL/environments?name=$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME" | "$WERCKER_STEP_ROOT/jq" '.data[0].id' | sed s/\"//g; }
 
 DTR_ENV_ID=$(get_env_id)
-echo "$DTR_ENV_ID"
+#echo "$DTR_ENV_ID"
 
 # get zip
 cd "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME"
@@ -39,22 +39,19 @@ wget -O file.zip "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY:$WERCKER_DE
 unzip file.zip
 
 # get old suffix
-echo "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME"
+#echo "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME"
 function get_old_service_name { sed -n "s/^\($WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME[^:]*\):[\r\n]\?$/\1/p" docker-compose.yml; }
 
 DTR_OLD_SERVICE_NAME=$(get_old_service_name)
-echo "$DTR_OLD_SERVICE_NAME"
+#echo "$DTR_OLD_SERVICE_NAME"
 
 # update docker-compose.yml to include new service name
 sed -i "s/^$DTR_OLD_SERVICE_NAME:/$DTR_OLD_SERVICE_NAME:\r\n$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME-$DTR_SUFFIX:/g" docker-compose.yml
 sed -i "s/^$DTR_OLD_SERVICE_NAME:/$DTR_OLD_SERVICE_NAME:\r\n$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME-$DTR_SUFFIX:/g" rancher-compose.yml
 sed -i "s/^\(\s *image: $WERCKER_DEPLOY_TO_RANCHER_DOCKER_ORG\/$WERCKER_DEPLOY_TO_RANCHER_DOCKER_IMAGE\).*$/\1:$WERCKER_DEPLOY_TO_RANCHER_TAG/g" docker-compose.yml
 
-# For initial testing
-cat docker-compose.yml
-cat rancher-compose.yml
-
 #do the deploy!
-#echo "$WERCKER_STEP_ROOT/rancher-compose" upgrade "$DTR_OLD_SERVICE_NAME" "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME-$DTR_SUFFIX" --pull --update-links -c --interval 30000 --url "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_RANCHER_URL" --access-key "$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY" --secret-key "$WERCKER_DEPLOY_TO_RANCHER_SECRET_KEY" --project-name "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME"
+# Echo the command cause it looks nice!
+echo "$WERCKER_STEP_ROOT/rancher-compose" --url "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_RANCHER_URL" --access-key "$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY" --secret-key "$WERCKER_DEPLOY_TO_RANCHER_SECRET_KEY" --project-name "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME" upgrade "$DTR_OLD_SERVICE_NAME" "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME-$DTR_SUFFIX" --pull --update-links -c --interval 30000
 
 "$WERCKER_STEP_ROOT/rancher-compose" --url "$DTR_PROTO://$WERCKER_DEPLOY_TO_RANCHER_RANCHER_URL" --access-key "$WERCKER_DEPLOY_TO_RANCHER_ACCESS_KEY" --secret-key "$WERCKER_DEPLOY_TO_RANCHER_SECRET_KEY" --project-name "$WERCKER_DEPLOY_TO_RANCHER_STACK_NAME" upgrade "$DTR_OLD_SERVICE_NAME" "$WERCKER_DEPLOY_TO_RANCHER_SERVICE_NAME-$DTR_SUFFIX" --pull --update-links -c --interval 30000
